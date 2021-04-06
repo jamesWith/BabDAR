3108f16f40fdfe6eea7408c56936372928c43c80
jamesWith <72391206+jamesWith@users.noreply.github.com>
2021-04-06 10:36:29 +0100

    Update Actiontubefuncs.py

--- FaZe/Actiontubefuncs.py
+++ FaZe/Actiontubefuncs.py
@@ -121,9 +121,9 @@ def getbucketcrop(bucket, frame):
 def getbucketnumbers(bucketlist, cap):
 	bucketdict = {-1: "-1"}
 	bucketwait = {}
-	print('hello')
-	if args.colab:
-		from IPython.display import Image
+	print('hellooo')
+	#if args.colab:
+		#from IPython.display import Image
 	for framenum, bucketperframe in enumerate(bucketlist):
 		for bucket in bucketperframe:
 			if bucket[4] not in bucketdict:
