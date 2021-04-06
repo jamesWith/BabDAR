c73ca3e919386489a7f755257a24ab96b0aa0485
jamesWith <72391206+jamesWith@users.noreply.github.com>
2021-04-06 11:42:29 +0100

    Update SetupBabDAR.sh

--- SetupBabDAR.sh
+++ SetupBabDAR.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#Move into the Darknet folder to make darknet
+
 
 
 conda env create -f BabDAR-venv.yml
@@ -10,27 +10,24 @@ conda activate BabDARvenv
 
 echo $CONDA_PREFIX
 
-
+#Move into the Darknet folder to make darknet
 cd Darknet
 while true; do
     read -p "Is a GPU available on this computer?" yn
     case $yn in
-        [Yy]* ) sed -i 's/OPENCV=0/OPENCV=1/' Makefile # change makefile to have GPU and OPENCV enabled (only if GPU is available)
-				sed -i 's/GPU=0/GPU=1/' Makefile
-				sed -i 's/CUDNN=0/CUDNN=1/' Makefile
-				sed -i 's/CUDNN_HALF=0/CUDNN_HALF=1/' Makefile; break;;
-        [Nn]* ) break;;
+        [Yy]* ) sed -i '' -e 's/OPENCV=0/OPENCV=1/' Makefile # change makefile to have GPU and OPENCV enabled (only if GPU is available)
+				sed -i '' -e 's/GPU=0/GPU=1/' Makefile
+				sed -i '' -e 's/CUDNN=0/CUDNN=1/' Makefile
+				sed -i '' -e 's/CUDNN_HALF=0/CUDNN_HALF=1/' Makefile; break;;
+        [Nn]* ) sed -i '' -e 's/OPENCV=1/OPENCV=0/' Makefile # change makefile to have GPU and OPENCV enabled (only if GPU is available)
+                sed -i '' -e 's/GPU=1/GPU=0/' Makefile
+                sed -i '' -e 's/CUDNN=1/CUDNN=0/' Makefile
+                sed -i '' -e 's/CUDNN_HALF=1/CUDNN_HALF=0/' Makefile; break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
 
 # make darknet
-make -q
+make
 
 cd ..
-
-#pip install filterpy==1.4.5
-#pip install scikit-image==0.17.2
-#pip install lap==0.4.0
-#pip install -q xlrd
-#pip install torchcam
