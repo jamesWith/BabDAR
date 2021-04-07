b510a0f2f6c0c558d83d9ed952ceb4b049dcf702
jamesWith <72391206+jamesWith@users.noreply.github.com>
2021-04-07 17:53:17 +0100

    Update Actiondetect.py

--- FaZe/Actiondetect.py
+++ FaZe/Actiondetect.py
@@ -300,7 +300,8 @@ def Run_detection(crop, action_label):
 		#	print('%-22s %0.2f'% (action_label[str(int(i))], final_scores[int(i)]))
 		#print('<----------------->')
 		frames = []
-	return detected_action, scores_indcies[:2]
+		return detected_action, scores_indcies[:2]
+	return detected_action, []
 
 
 def Detect(filename):
