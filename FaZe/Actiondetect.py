fb0a048a6bd638be77cfe057b905cf12c2a5979f
jamesWith <72391206+jamesWith@users.noreply.github.com>
2021-04-08 13:46:51 +0100

    Update Actiondetect.py

--- FaZe/Actiondetect.py
+++ FaZe/Actiondetect.py
@@ -331,7 +331,7 @@ def Detect(filename):
 		for framenum, detections in enumerate(baboonlist[startframe::], start = startframe):
 			ret, currentframe = cap.read()
 			if ret:
-				print(framenum)
+				print(detections)
 				preframes.append(currentframe)
 				if len(preframes) > 6:
 					preframes.pop(0)
